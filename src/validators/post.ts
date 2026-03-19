1875e7c03b172a975c2e7f7eab97d8f353dd6650
Terrance Summers <terrancesummers@Terrances-MacBook-Pro.local>
2026-03-18 23:47:22 -0500

    fixed post action

--- src/validators/post.ts
+++ src/validators/post.ts
@@ -1,13 +1,14 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const createPostSchema = z.object({
   authorUserId: z.string(),
-  type: z.enum(["text", "media", "disscussion", "highlight"]),
+  type: z.enum(['text', 'media', 'disscussion', 'highlight']),
   text: z.string().optional(),
-  mediaUrl: z.string().optional(),
-  topic: z.string.optional(),
+  mediaURL: z.string().optional(),
+  topic: z.string().optional(),
+  sportTag: z.string().optional(),
   relatedPickupGameId: z.string().optional(),
-  visibilty: z.enum(["public", "friends", "private"])
+  visibility: z.enum(['public', 'friends', 'private']),
 });
 
 export type CreatePostBody = z.infer<typeof createPostSchema>;
